7b7ed09f5a6e3668fa6536e14f3600c334ef2cb9
Adejuwon-00 <juwonmcalpha@gmail.com>
2022-02-17 23:15:39 +0100

    new commit

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import SignIn from "./Illustrations/SignIn.svg";
+import SignUp from "./Illustrations/SignUp.svg";
+
+export * from "./icons/CloseIcon";
+export const Illustrations = {
+  SignUp,
+  SignIn
+};
